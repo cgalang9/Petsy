10d2de1bd70d4f728651e55f213b4cd0bd5bf24b
cgalang <cgalang11@hotmail.com>
2022-11-11 14:00:56 -0800

    Add seed data for users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,37 @@ from app.models import db, User, environment, SCHEMA
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    mike = User(
+        username="Mike's Pet Emporium", email='mike@aa.io', password='password123')
+    johnny = User(
+        username="Johnny's cash exchange", email='johnny@aa.io', password='password')
+    kat = User(
+        username="Kat's cat stuff", email='kat@aa.io', password='password')
+    jumbo = User(
+        username='Mumbo Jumbo Pet Supplies', email='jumbo@aa.io', password='password')
+    bo = User(
+        username='Bo Knows Dogs', email='bo@aa.io', password='password')
+    tarot = User(
+        username="Tarot's for Parrots", email='tarot@aa.io', password='password')
+    sanford = User(
+        username='Sanford and Sons Pet Supplies', email='sanford@aa.io', password='password')
+    miller = User(
+        username='Miller Pets', email='miller@aa.io', password='password')
+    joe = User(
+        username="Joe Exotic's Tiger Supplies", email='joe@aa.io', password='password')
+    cozey = User(
+        username='Cozey Critters', email='cozey@aa.io', password='password')
+    pitbull = User(
+        username="Mr Worldwide's Pitbull Store", email='pitbull@aa.io', password='password')
+    chris = User(
+        username="Chris' Cattery", email='chris@aa.io', password='password')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
-    db.session.commit()
+    users = [demo, mike, johnny, kat, jumbo, bo, tarot, sanford, miller, cozey, joe, pitbull, chris]
+
+    for user in users:
+        db.session.add(user)
+        db.session.commit()
+
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
@@ -27,5 +49,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
