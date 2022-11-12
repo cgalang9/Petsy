a32d8a3ad57cef5459f6e66033175b79218d498a
jason <jh.premo@gmail.com>
2022-11-12 10:27:12 -0500

    post review image route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from flask_login import current_user
-from app.models import Review, db
+from app.models import Review, ReviewImage, db
 
 review_routes = Blueprint('reviews', __name__)
 
@@ -12,6 +12,9 @@ def test_route():
 
 @review_routes.delete('/<int:id>')
 def delete_review(id):
+    """
+    deletes review by review id
+    """
     delete_review = Review.query.get(id)
     if not delete_review:
         return {"message": "Review not found"}, 404
@@ -21,3 +24,29 @@ def delete_review(id):
         return {"message": "Successfully deleted"}
     else:
         return {"message": "Forbidden"}, 403
+
+
+@review_routes.post('/<int:id>/images')
+def add_review_image(id):
+    """
+    posts a review image by review id
+    """
+    review = Review.query.get(id)
+    url = request.json.get('url')
+    if not review:
+        return {"message": "Review not found"}, 404
+    elif not (current_user.is_authenticated and review.user_id == current_user.id):
+        return {"message": "Forbidden"}, 403
+    elif not url or '.' not in url:
+        return ({"message": "validation error",
+        "errors": {
+            "url": "url must be valid"
+        }}, 400)
+    else:
+        new_review_image = ReviewImage(
+            url=url,
+            review_id=review.id
+            )
+        db.session.add(new_review_image)
+        db.session.commit()
+        return {"message": "image successfully created"}
