f77a6de9575dda61c6926a3d0644049bf922abca
cgalang <cgalang11@hotmail.com>
2022-11-12 10:38:17 -0800

    Added create item route

--- app/api/item_routes.py
+++ app/api/item_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
-from flask_login import login_required, current_user
+from flask import Blueprint, request
+from flask_login import login_required, current_user, login_user
 from sqlalchemy.orm import joinedload
-from ..models import db, Product, Review, OrderProduct
+from ..models import db, Product, Review, OrderProduct, User
 from ..forms.create_item_form import CreateProductForm
 
 item_routes = Blueprint('items', __name__)
@@ -13,49 +13,55 @@ def test_route():
 
 
 @item_routes.post('/')
-# @login_required
+@login_required
 def create_item():
+    """
+    Creates/posts a new item
+    """
     form = CreateProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        return 'hello'
+        new_product = Product(
+            user_id=current_user.get_id(),
+            name=form.data['name'],
+            price=form.data['price'],
+            description=form.data['description']
+        )
+        db.session.add(new_product)
+        db.session.commit()
+
+        #Gets all reviews of store then calculates avg rating
+        reviews = Review.query.join(Product).filter(Product.user_id == current_user.get_id()).options(joinedload(Review.product)).all()
+        avg_rating = 0
+        for review in reviews:
+            avg_rating += review.rating
+        avg_rating /= len(reviews)
+
+        #Gets all reviews of store then calculates number of sales
+        orders = OrderProduct.query.join(Product).filter(Product.user_id == current_user.get_id()).options(joinedload(OrderProduct.product)).all()
+        sales = 0
+        for order in orders:
+            sales += order.quantity
+
+        prod = {
+            "id": new_product.id,
+            "sellerId": current_user.get_id(),
+            "name": new_product.name,
+            "shopName": current_user.username,
+            "price": new_product.price,
+            "avgShopRating": avg_rating,
+            "shopSales": sales,
+            "description": new_product.description,
+            "shopReviews": len(reviews),
+            "itemReviews": 0,
+            "imageURLs": []
+        }
+
+        return prod
     else:
-        print(form.name)
-        return 'byesd'
-        # new_product = Product(
-        #     user_id=current_user.id,
-        #     name=form.data['name'],
-        #     price=form.data['price'],
-        #     description=form.data['description']
-        # )
-        # db.session.add(new_product)
-        # db.session.commit()
-        # final_prod = Product.query.options(joinedload(Product.user)).get(new_product.user_id)
-        # reviews = Review.query.options(joinedload(Review.product)).filter(Review.product.user_id == new_product.user_id)
-        # orders = OrderProduct.query.options(joinedload(OrderProduct.product)).filter(OrderProduct.product.user_id == new_product.user_id)
-
-        # avg_rating = 0
-        # for review in reviews:
-        #     avg_rating += review.rating
-        # avg_rating /= len(reviews)
-
-        # sales = 0
-        # for order in orders:
-        #     sales += order.quanitity
+        return 'error'
 
 
-        # prod = {
-        #     "id": final_prod.id,
-        #     "sellerId": final_prod.user_id,
-        #     "name": final_prod.name,
-        #     "shopName": final_prod.user.name,
-        #     "price": final_prod.price,
-        #     "avgShopRating": avg_rating,
-        #     "shopSales": sales,
-        #     "description": final_prod.description,
-        #     "shopReviews": len(reviews),
-        #     "itemReviews": 0,
-        #     "imageURLs": []
-        # }
         # return prod
     # else:
     #     return '<h1>Bad Data</h1>'
--- app/forms/create_item_form.py
+++ app/forms/create_item_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, FloatField, SubmitField, TextAreaField
+from wtforms.fields import StringField, FloatField, TextAreaField
 from wtforms.validators import Length, DataRequired, NumberRange
 
 
@@ -7,5 +7,4 @@ class CreateProductForm(FlaskForm):
     name = StringField('Name', validators=[Length(min=1, max=75), DataRequired()])
     price = FloatField('Price', validators=[NumberRange(min=0), DataRequired()])
     description = StringField('Description', validators=[Length(min=1, max=2000), DataRequired()])
-    images = TextAreaField('Image URLs (sperated by comma)')
-    submit = SubmitField('Submit')
+    # images = TextAreaField('Image URLs (sperated by comma)')
