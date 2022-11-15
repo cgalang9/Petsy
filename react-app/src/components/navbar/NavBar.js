a85e918346a852ab2c879de20768db57c47ec6d9
jason <jh.premo@gmail.com>
2022-11-15 08:57:21 -0500

    completed search bar functionality

--- react-app/src/components/navbar/NavBar.js
+++ react-app/src/components/navbar/NavBar.js
@@ -1,19 +1,33 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import ProfileButton from './profileButton';
 import './navbar.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, } from 'react';
 
 
 const NavBar = () => {
 
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState(null)
   const [toggleAdvancedSearch, setToggleAdvancedSearch] = useState(false)
   const [pageSize, setPageSize] = useState(null)
-  const [pageNum, setPageNum] = useState(null)
   const [minPrice, setMinPrice] = useState(null)
   const [maxPrice, setMaxPrice] = useState(null)
+  const history = useHistory()
+  const submitSearch = () => {
+
+    let searchParams = new URLSearchParams()
+    if (searchTerm) searchParams.append('q', searchTerm)
+    if (minPrice) searchParams.append('minPrice', minPrice)
+    if (maxPrice) searchParams.append('maxPrice', maxPrice)
+    if (pageSize) searchParams.append('pageSize', pageSize)
+    if (searchParams.toString()) {
+      history.push('/search?' + searchParams.toString())
+    } else {
+      history.push('/')
+    }
+  }
+
 
   // function to open advanced search drop down
   const openAdvancedSearch = () => {
@@ -23,6 +37,7 @@ const NavBar = () => {
   let advancedSearchClass
   if (toggleAdvancedSearch) advancedSearchClass = 'navbar-advanced-search-dropdown-wrapper'
   else advancedSearchClass = 'navbar-hidden'
+
 
   return (
     <nav className='navbar-wrapper'>
@@ -36,15 +51,19 @@ const NavBar = () => {
         </button>
         <div className={advancedSearchClass}>
           <div>
-            <label>Minimum price</label>
-            <input type='number' min='0' value={minPrice} onChange={() => setMinPrice}></input>
+            <label>Minimum price: </label>
+            <input className='navbar-advanced-search-dropdown-input' type='number' min='0' value={minPrice} onChange={(e) => setMinPrice(e.target.value)}></input>
           </div>
           <div>
-            <label>Maximum price</label>
-            <input type='number' min='0' value={maxPrice} onChange={() => setMaxPrice}></input>
+            <label>Maximum price: </label>
+            <input className='navbar-advanced-search-dropdown-input' type='number' min={`${minPrice}`} value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)}></input>
+          </div>
+          <div>
+            <label>Results per page: </label>
+            <input className='navbar-advanced-search-dropdown-input' type='number' min='0' value={pageSize} onChange={(e) => setPageSize(e.target.value)}></input>
           </div>
         </div>
-        <button className='navbar-search-button'>
+        <button className='navbar-search-button' onClick={submitSearch}>
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
       </div>
