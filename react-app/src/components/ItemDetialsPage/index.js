2f049fd9320f526efd6b997aca645acff7d642f6
cgalang <cgalang11@hotmail.com>
2022-11-15 16:17:14 -0800

    Add css and route to get all review images by review id

--- react-app/src/components/ItemDetialsPage/index.js
+++ react-app/src/components/ItemDetialsPage/index.js
@@ -106,90 +106,94 @@ function ItemDetailsPage() {
     }
 
     return (
-        <>
-        {isLoaded && (
-            <div id='items-details-page'>
-                {item && (
-                    <>
-                        <div id='items-details-page-left'>
-                            <div id='items-details-page-images-container'>
-                                <div id='items-details-page-images-container-tiles'>
-                                    {item.imageURLs && (item.imageURLs.map((url, idx) => (
-                                        <div key={idx} >
-                                            <img
-                                                src={url}
-                                                alt="item picture"
-                                                id={`img-page-tile-${idx}`}
-                                                className={idx == 0 ? "items-details-page-images-container-tiles-images active-tile-image" : "items-details-page-images-container-tiles-images"}
-                                                onClick={makeActive}
-                                            ></img>
+        <div id='items-details-page-oustside-container'>
+            {isLoaded && (
+                <div id='items-details-page'>
+                    {item && (
+                        <>
+                            <div id='items-details-page-left'>
+                                <div id='items-details-page-images-container'>
+                                    <div id='items-details-page-images-container-tiles'>
+                                        {item.imageURLs && (item.imageURLs.map((url, idx) => (
+                                            <div key={idx} >
+                                                <img
+                                                    src={url}
+                                                    alt="item picture"
+                                                    id={`img-page-tile-${idx}`}
+                                                    className={idx == 0 ? "items-details-page-images-container-tiles-images active-tile-image" : "items-details-page-images-container-tiles-images"}
+                                                    onClick={makeActive}
+                                                ></img>
+                                            </div>
+                                        )))}
+                                    </div>
+                                    <div id='items-details-page-images-container-main'>
+                                        <div className='items-details-page-arrow' onClick={handleLeftArrow}><i className="fa-solid fa-angle-left" /></div>
+                                        {item.imageURLs && (item.imageURLs.map((url, idx) => (
+                                            <div key={idx}>
+                                                <img
+                                                    src={url}
+                                                    alt="item picture"
+                                                    id={`img-page-main-${idx}`}
+                                                    className={idx == 0 ? "items-details-page-images-container-main-images show" : "items-details-page-images-container-main-images"}
+                                                ></img>
+                                            </div>
+                                        )))}
+                                        <div className='items-details-page-arrow' onClick={handleRightArrow}><i className="fa-solid fa-angle-right" /></div>
+                                    </div>
+                                </div>
+                                <div id='items-details-page-main-review-containter'>
+                                    <div id='items-details-page-main-shop-reviews'>
+                                        {item.shopReviews} reviews for this store
+                                        <span className='items-details-page-stars'><StarRatings rating={item.avgShopRating} starRatedColor="black" numberOfStars={5} starDimension='25px' starSpacing='1px' /></span>
+                                    </div>
+                                    <div id='items-details-page-main-item-reviews-total'>Reviews for this item <span>{itemReviews.length}</span></div>
+                                    {itemReviews && (itemReviews.slice(reviewIdx, reviewIdx + 4).map(review => (
+                                        <div key={review.id} className='items-details-page-review-containter'>
+                                            <div className='items-details-page-main-item-reviews-rating'><StarRatings rating={review.starRating} starRatedColor="black" numberOfStars={5} starDimension='20px' starSpacing='1px' /></div>
+                                            <div className='items-details-page-main-item-reviews-text'>{review.text}</div>
+                                            <div className='items-details-page-main-item-reviews-user'>{review.user.username} {new Date(review.date).toDateString().slice(4)}</div>
                                         </div>
                                     )))}
+                                    <div id='items-details-page-main-item-reviews-page'>
+                                        <span className='items-details-page-arrow-review' onClick={handleLeftArrowReview}><i className="fa-solid fa-angle-left" /></span>
+                                        Page {reviewIdx / 4 + 1} of {Math.ceil(itemReviews.length / 4)}
+                                        <span className='items-details-page-arrow-review' onClick={handleRightArrowReview}><i className="fa-solid fa-angle-right" /></span>
+                                    </div>
                                 </div>
-                                <div id='items-details-page-images-container-main'>
-                                    <div className='items-details-page-arrow' onClick={handleLeftArrow}><i className="fa-solid fa-angle-left" /></div>
-                                    {item.imageURLs && (item.imageURLs.map((url, idx) => (
-                                        <div key={idx}>
-                                            <img
-                                                src={url}
-                                                alt="item picture"
-                                                id={`img-page-main-${idx}`}
-                                                className={idx == 0 ? "items-details-page-images-container-main-images show" : "items-details-page-images-container-main-images"}
-                                            ></img>
+                            </div>
+                            <div id='items-details-page-right'>
+                                <div id='items-details-page-right-shopname'>{item.shopName}</div>
+                                <div id='items-details-page-right-shop-sales'>
+                                    {item.shopSales} sales
+                                    <span> | </span>
+                                    <StarRatings rating={item.avgShopRating} starRatedColor="black" numberOfStars={5} starDimension='20px' starSpacing='1px' />
+                                </div>
+                                {sessionUser && sessionUser.id === item.sellerId && (
+                                    <div id='items-details-page-edit-links'>
+                                        <div id='edit-item-link'>
+                                            <NavLink to={{
+                                                pathname: `/items/${itemId}/edit-item`,
+                                                state: { ...item }
+                                            }}> Edit Item
+                                            </NavLink>
                                         </div>
-                                    )))}
-                                    <div className='items-details-page-arrow' onClick={handleRightArrow}><i className="fa-solid fa-angle-right" /></div>
+                                        <div id='delete-item-link' onClick={handleDelete}>
+                                            Delete Item
+                                        </div>
+                                    </div>
+                                )}
+                                <div id='items-details-page-right-item-name'>{item.name}</div>
+                                <div id='items-details-page-right-price'>${item.price}</div>
+                                <div id='items-details-page-right-description'>
+                                    <div id='items-details-page-right-description-head'>Description</div>
+                                    <div id='items-details-page-right-description-text'>{item.description}</div>
                                 </div>
                             </div>
-                            <div id='items-details-page-main-review-containter'>
-                                <div id='items-details-page-main-shop-reviews'>
-                                    {item.shopReviews} reviews for this store
-                                    <span className='items-details-page-stars'><StarRatings rating={item.avgShopRating} starRatedColor="black" numberOfStars={5} starDimension='25px' starSpacing='1px' /></span>
-                                </div>
-                                <div id='items-details-page-main-item-reviews-total'>Reviews for this item <span>{itemReviews.length}</span></div>
-                                {itemReviews && (itemReviews.slice(reviewIdx, reviewIdx + 4).map(review => (
-                                    <div key={review.id} className='items-details-page-review-containter'>
-                                        <div className='items-details-page-main-item-reviews-rating'><StarRatings rating={review.starRating} starRatedColor="black" numberOfStars={5} starDimension='20px' starSpacing='1px' /></div>
-                                        <div className='items-details-page-main-item-reviews-text'>{review.text}</div>
-                                        <div className='items-details-page-main-item-reviews-user'>{review.user.username} {new Date(review.date).toDateString().slice(4)}</div>
-                                    </div>
-                                )))}
-                                <div id='items-details-page-main-item-reviews-page'>
-                                    <span className='items-details-page-arrow-review' onClick={handleLeftArrowReview}><i className="fa-solid fa-angle-left" /></span>
-                                    Page {reviewIdx / 4 + 1} of {Math.ceil(itemReviews.length / 4)}
-                                    <span className='items-details-page-arrow-review' onClick={handleRightArrowReview}><i className="fa-solid fa-angle-right" /></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div id='items-details-page-right'>
-                            <div id='items-details-page-right-shopname'>{item.shopName}</div>
-                            <div>
-                                {item.shopSales} sales
-                                | <StarRatings rating={item.avgShopRating} starRatedColor="black" numberOfStars={5} starDimension='20px' starSpacing='1px' />
-                            </div>
-                            {sessionUser && sessionUser.id === item.sellerId && (
-                                <>
-                                    <div id='edit-item-link'>
-                                        <NavLink to={{
-                                            pathname: `/items/${itemId}/edit-item`,
-                                            state: { ...item }
-                                        }}> Edit Item
-                                        </NavLink>
-                                    </div>
-                                    <div id='delete-item-link' onClick={handleDelete}>
-                                        Delete Item
-                                    </div>
-                                </>
-                            )}
-                            <div>{item.name}</div>
-                            <div>${item.price}</div>
-                            <div>{item.description}</div>
-                        </div>
-                    </>
-                )}
-            </div>
-        )}
-        </>
+                        </>
+                    )}
+                </div>
+            )}
+        </div>
     )
 }
 
