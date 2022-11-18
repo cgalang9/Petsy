72a85c5f7ca10b9f4453c88f0a62187b9f1ec45b
Jwad <jwad.aziz@gmail.com>
2022-11-18 08:53:15 -0800

    finished email validation on signup

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -3,13 +3,13 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
-
 def user_exists(form, field):
     # Checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
     if not user:
         raise ValidationError('Email provided not found.')
+    
 
 
 def password_matches(form, field):
