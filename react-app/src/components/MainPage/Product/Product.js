cd517511adff08594e65260b9895664d4d62843c
Jwad <jwad.aziz@gmail.com>
2022-11-18 09:14:18 -0800

    added star ratings to front paaaaaage

--- react-app/src/components/MainPage/Product/Product.js
+++ react-app/src/components/MainPage/Product/Product.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useHistory } from "react-router-dom"
+import StarRatings from "react-star-ratings";
 import "./Product.css"
 
 export default function Product( { product, id }) {
@@ -25,7 +26,16 @@ export default function Product( { product, id }) {
             <div className={`product-image ${hovered}`} style={backgroundStyle}/>
             <div className="product-details">
                 <div className="product-name">{name}</div>
-                <div>{avgShopRating} ({shopReviews})</div>
+                <div>
+                    <StarRatings
+                      rating={avgShopRating}
+                      starRatedColor="black"
+                      numberOfStars={5}
+                      starDimension="15px"
+                      starSpacing="1px"
+                    />
+                    ({shopReviews})
+                </div>
                 <div className="product-price">${price.toFixed(2)}</div>
                 <div className="product-shop-name">{shopName}</div>
             </div>
